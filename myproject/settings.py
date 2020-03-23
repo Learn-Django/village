3abe27216bb2e6115d345ed5bb6881477add9fa3
Havenly Yaas <62045011+havenlyyaas@users.noreply.github.com>
2020-03-23 23:26:26 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS    = '/home/charles/project1/myproject/myapp/templates/static',
